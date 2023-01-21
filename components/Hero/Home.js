e0c7ef73eb24c97dd2e73b31d9bb9f45c4dfd484
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-01-20 19:37:19 -0500

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -42,17 +42,17 @@ const Hero = ({ blockMap }) => {
           <Social />
           <div className='flex flex-col sm:flex-row sm:justify-center gap-4 mt-6'>
             <Link passHref href='/contact' scroll={false}>
-              {/*
               <button className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'>
                 <MailIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
-                <span className='ml-4 flex items-start flex-col leading-none'>
+                  {/*
+                 <span className='ml-4 flex items-start flex-col leading-none'>
                   <span className='text-xs text-gray-600 dark:text-day mb-1'>
                     {t.HERO.HOME.CONTACT_BUTTON_DES}
                   </span>
                   <span className='font-medium'>{t.HERO.HOME.CONTACT_BUTTON}</span>
                 </span>
+                */}
               </button>
-              */}
             </Link>
             {showCopied ? (
               <button
