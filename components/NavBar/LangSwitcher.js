c4fb5a4d93da559f2f2ae2572676e19a10c38462
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-01-20 21:40:23 -0500

    Update LangSwitcher.js

--- components/NavBar/LangSwitcher.js
+++ components/NavBar/LangSwitcher.js
@@ -12,7 +12,7 @@ const LangSwitcher = () => {
           aria-label='LangSwitcher'
           className=''
         >
-          <TranslateIcon className='h-5 w-5' />
+          <TranslateIcon className='' />
         </button>
       </Link>
     </>
