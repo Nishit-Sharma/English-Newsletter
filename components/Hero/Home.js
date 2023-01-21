41f4192f7e883edbb01732f592527121c342a907
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-01-20 19:31:05 -0500

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -6,7 +6,7 @@ import { lang } from '@/lib/lang'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import {
-  MailIcon,
+  {/*MailIcon,*/}
   RssIcon,
   ClipboardCheckIcon
 } from '@heroicons/react/outline'
