94505be66d4f93d05b9bdf1ac7c08378dda4f3e3
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-01-20 19:53:11 -0500

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -41,7 +41,7 @@ const Hero = ({ blockMap }) => {
             recordMap={blockMap}
             components={{ Collection }}
           />
-          <Social />
+{/* <Social /> */}
           <div className='flex flex-col sm:flex-row sm:justify-center gap-4 mt-6'>
             <Link passHref href='' scroll={false}>
               <button className=''>
