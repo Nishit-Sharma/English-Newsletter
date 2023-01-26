b493ebc553e0a14a4e4776802c536b5bd2b3f0df
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-01-26 10:06:27 -0500

    Update Newsletter.js

--- components/Hero/Newsletter.js
+++ components/Hero/Newsletter.js
@@ -28,7 +28,7 @@ const NewsletterHero = ({ blockMap }) => {
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
-        <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 md:text-left'>
+        <div className=''>
           <NotionRenderer
             className='md:ml-0'
             recordMap={blockMap}
