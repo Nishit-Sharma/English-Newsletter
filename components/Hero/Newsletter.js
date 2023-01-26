c2fb4ebde86410b3842cc876cb634cafc03053ad
Nishit-Sharma <102970864+Nishit-Sharma@users.noreply.github.com>
2023-01-26 10:08:01 -0500

    Update Newsletter.js

--- components/Hero/Newsletter.js
+++ components/Hero/Newsletter.js
@@ -28,7 +28,7 @@ const NewsletterHero = ({ blockMap }) => {
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
-        <div className=''>
+        <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 md:text-left'>
           <NotionRenderer
             className='md:ml-0'
             recordMap={blockMap}
@@ -41,12 +41,12 @@ const NewsletterHero = ({ blockMap }) => {
 
           <div className=''>
             <Link passHref href={BLOG.telegramChannelUrl} scroll={false}>
-              <button className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'>
+              <button className=''>
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   fill='currentColor'
-                  className='text-gray-600 dark:text-day w-7 h-7'
-                  viewBox='0 0 24 24'
+                  className=''
+                  viewBox=''
                 >
                   <path fill='none' d='M0 0h24v24H0z' />
                   <path d='M12 20a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0 2C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-3.11-8.83l-2.498-.779c-.54-.165-.543-.537.121-.804l9.733-3.76c.565-.23.885.061.702.79l-1.657 7.82c-.116.557-.451.69-.916.433l-2.551-1.888-1.189 1.148c-.122.118-.221.219-.409.244-.187.026-.341-.03-.454-.34l-.87-2.871-.012.008z' />
